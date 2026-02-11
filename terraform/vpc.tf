f235214a8cbb08cac073456c772e1becb3f10c92
Engr.Jimmy <ndumeleikenna@gmail.com>
2026-02-11 14:56:58 +0100

    Create vpc.tf

---
+++ terraform/vpc.tf
@@ -0,0 +1,22 @@
+resource "aws_vpc" "this" {
+  cidr_block = var.vpc_cidr
+  tags = {
+    Name = "${var.project_name}-vpc"
+  }
+}
+
+resource "aws_subnet" "public" {
+  vpc_id            = aws_vpc.this.id
+  cidr_block        = var.public_subnet_cidr
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "${var.project_name}-public-subnet"
+  }
+}
+
+resource "aws_internet_gateway" "this" {
+  vpc_id = aws_vpc.this.id
+  tags = {
+    Name = "${var.project_name}-igw"
+  }
+}
