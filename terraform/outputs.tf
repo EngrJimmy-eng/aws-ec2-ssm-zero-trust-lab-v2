4fd849ba6def9fa85132d0a2596315039f879cd9
Engr.Jimmy <ndumeleikenna@gmail.com>
2026-02-11 14:58:47 +0100

    Create outputs.tf

---
+++ terraform/outputs.tf
@@ -0,0 +1,24 @@
+output "vpc_id" {
+  description = "VPC ID"
+  value       = aws_vpc.this.id
+}
+
+output "subnet_id" {
+  description = "Public subnet ID"
+  value       = aws_subnet.public.id
+}
+
+output "instance_id" {
+  description = "EC2 instance ID"
+  value       = aws_instance.this.id
+}
+
+output "instance_public_ip" {
+  description = "EC2 public IP"
+  value       = aws_instance.this.public_ip
+}
+
+output "instance_profile_arn" {
+  description = "EC2 IAM instance profile ARN"
+  value       = aws_iam_instance_profile.this.arn
+}
