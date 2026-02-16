05c945ccaaba977e9ba61dce4128718ea0db71ae
Engr.Jimmy <ndumeleikenna@gmail.com>
2026-02-16 08:01:06 +0100

    Update iam.tf

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -22,3 +22,44 @@ resource "aws_iam_instance_profile" "ec2_profile" {
   name = "ec2-ssm-instance-profile"
   role = aws_iam_role.ec2_ssm_role.name
 }
+
+resource "aws_iam_role" "firehose_role" {
+  name = "firehose_to_s3_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "firehose.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "firehose_policy" {
+  role = aws_iam_role.firehose_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:PutObject",
+          "s3:PutObjectAcl",
+          "s3:ListBucket"
+        ]
+        Resource = [
+          aws_s3_bucket.log_archive.arn,
+          "${aws_s3_bucket.log_archive.arn}/*"
+        ]
+      }
+    ]
+  })
+}
+
+
