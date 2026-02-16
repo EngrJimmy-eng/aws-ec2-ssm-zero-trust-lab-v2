8d680915a912333ce83908ba115a59f7eb0c1f5c
Engr.Jimmy <ndumeleikenna@gmail.com>
2026-02-16 08:50:22 +0100

    Update logging.tf

--- terraform/logging.tf
+++ terraform/logging.tf
@@ -15,9 +15,9 @@ resource "aws_s3_bucket_public_access_block" "block_public" {
 
 resource "aws_kinesis_firehose_delivery_stream" "cw_to_s3" {
   name        = "cw-to-s3-stream"
-  destination = "s3"
+  destination = "extended_s3"
 
-  s3_configuration {
+  extended_s3_configuration {
     role_arn           = aws_iam_role.firehose_role.arn
     bucket_arn         = aws_s3_bucket.log_archive.arn
     buffering_size     = 1
