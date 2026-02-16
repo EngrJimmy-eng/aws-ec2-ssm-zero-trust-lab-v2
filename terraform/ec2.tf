db04ae538242a894afc2f58fa9ac801b80a3a537
Engr.Jimmy <ndumeleikenna@gmail.com>
2026-02-16 08:43:48 +0100

    Update ec2.tf

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -31,7 +31,7 @@ resource "aws_instance" "ec2_ssm" {
   instance_type               = "t3.micro"
   subnet_id                   = aws_subnet.public.id
   vpc_security_group_ids      = [aws_security_group.ec2_sg.id]
-  instance_state = "stopped"
+  
   iam_instance_profile        = aws_iam_instance_profile.ec2_profile.name
   associate_public_ip_address = true
 
