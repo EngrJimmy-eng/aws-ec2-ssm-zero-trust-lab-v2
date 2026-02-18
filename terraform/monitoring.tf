35b66816b2bc5e1625f0f70bcbc8548920505037
Engr.Jimmy <ndumeleikenna@gmail.com>
2026-02-18 18:59:50 +0100

    Create monitoring.tf

---
+++ terraform/monitoring.tf
@@ -0,0 +1,46 @@
+resource "aws_cloudwatch_log_group" "vpc_flow_logs" {
+  name              = "/${var.project_name}/vpc-flow-logs"
+  retention_in_days = 7
+}
+
+resource "aws_iam_role" "vpc_flow_logs_role" {
+  name = "${var.project_name}-vpc-flow-logs-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "vpc-flow-logs.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "vpc_flow_logs_policy" {
+  role = aws_iam_role.vpc_flow_logs_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_flow_log" "vpc_flow_log" {
+  vpc_id               = aws_vpc.this.id
+  log_destination      = aws_cloudwatch_log_group.vpc_flow_logs.arn
+  traffic_type         = "ALL"
+  iam_role_arn         = aws_iam_role.vpc_flow_logs_role.arn
+}
