0b1575844d260bf9f135144fca48e7daf8550fe5
Engr.Jimmy <ndumeleikenna@gmail.com>
2026-02-19 18:42:08 +0100

    Update iam.tf

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -22,6 +22,28 @@ resource "aws_iam_instance_profile" "ec2_profile" {
   name = "ec2-ssm-instance-profile"
   role = aws_iam_role.ec2_ssm_role.name
 }
+resource "aws_iam_policy" "ec2_s3_limited" {
+  name = "${var.project_name}-ec2-s3-limited"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:PutObject",
+          "s3:GetObject"
+        ]
+        Resource = "arn:aws:s3:::${var.project_name}-*/*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ec2_s3_attach" {
+  role       = aws_iam_role.ec2_role.name
+  policy_arn = aws_iam_policy.ec2_s3_limited.arn
+}
 
 
 
