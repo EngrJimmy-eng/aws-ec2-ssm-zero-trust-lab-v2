55816e75f16b77501a4562202485458d0cbdec90
Engr.Jimmy <ndumeleikenna@gmail.com>
2026-02-25 08:50:56 +0100

    Update s3.tf

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -173,6 +173,16 @@ resource "aws_s3_bucket_lifecycle_configuration" "secure_logs_lifecycle" {
   }
 }
 
+resource "aws_s3_bucket_server_side_encryption_configuration" "secure_logs_encryption" {
+  bucket = aws_s3_bucket.secure_logs_bucket.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
 
 
 
